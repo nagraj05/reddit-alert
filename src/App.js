8d95ec45eb38e8e01b62ceb69743072d058b0ed3
Nagraj <tadingi.nagraj.nagraj7@gmail.com>
2023-05-11 20:41:54 +0530

    first upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function MainContent(){
+  return(
+    <div className="header">
+    <h1>Haha gotchu</h1>
+    <h3>You should've not clicked it!</h3>
+    <p>Stop clicking every link you find online,
+      it may be a virus, or you could get hacked!
+    </p>
+    <h4>You are safe now! Go Back</h4>
     </div>
-  );
+  )
+}
+
+function App(){
+  return(
+    <div>
+      <MainContent />
+    </div>
+  )
 }
 
 export default App;
